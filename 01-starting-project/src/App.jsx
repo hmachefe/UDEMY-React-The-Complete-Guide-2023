adf5e0bb0f3a266568ecd3f9d0fae672a94910e6
Hugo Machefer <hugo.machefer@difuze.com>
2023-10-29 18:37:04 -0400

    create core concepts component and pass props dynamically

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,6 +1,15 @@
 import reactImg from "./assets/react-core-concepts.png";
+import compomentsImg from "./assets/components.png";
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function CoreConcepts(props) {
+  return <li>
+    <img src={props.image} alt={props.title} />
+    <h3>{props.title}</h3>
+    <p>{props.description}</p>
+  </li>
+}
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -26,6 +35,24 @@ function App() {
     <div>
       <Header></Header>
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              title="Components"
+              description="Components are the building blocks of React apps"
+              image={compomentsImg}
+            />
+            <CoreConcepts
+              title="Props"
+              description="Data passed to components"
+            />
+            <CoreConcepts
+              title="State"
+              description="Data managed by a component"
+            />
+            </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
