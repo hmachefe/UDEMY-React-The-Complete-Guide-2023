5256b8dd9df82e48973e8bf6b18fb68b311b18e1
Hugo Machefer <hugo.machefer@difuze.com>
2023-11-05 20:16:12 -0500

    64--Problem--Props-Are-Not-Forwarded-To-Inner-Elements

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { EXAMPLES } from "../data";
 import TabButton from "./TabButton"
+import Section from "./Section";
 
 
 export function Examples() {
@@ -26,14 +27,13 @@ export function Examples() {
       </div>
     };
 
-    return <section id="examples">
-        <h2>Examples</h2>
+    return <Section id="examples" title="Examples">
         <menu>
-        <TabButton isSelected={selectedTopic === 'components'} onSelect={() => onSelect('components')}>Components</TabButton>
-        <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => onSelect('jsx')}>JSX</TabButton>
-        <TabButton isSelected={selectedTopic === 'props'} onSelect={() => onSelect('props')}>Props</TabButton>
-        <TabButton isSelected={selectedTopic === 'state'} onSelect={() => onSelect('state')}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => onSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => onSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => onSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => onSelect('state')}>State</TabButton>
         </menu>
         {tabContent}
-    </section>
+    </Section>
 }
