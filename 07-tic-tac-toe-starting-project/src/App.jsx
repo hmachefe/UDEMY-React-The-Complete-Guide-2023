57b714075754ef07e060f30b760edb2fa26cd3d4
Hugo Machefer <hugo.machefer@difuze.com>
2023-12-08 23:11:19 -0500

    81---lifting-state-up---core-concept

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,17 +1,28 @@
 import Player from "./components/Player"
-import GameBoard from "./components/GameBoard"
+import GameBoard from "./components/Gameboard"
+import { useState } from "react";
 
 function App() {
   
+  const [activeUser, setActiveUser] = useState('X');
+
+  function handleSquareClick(playerSymbol) {
+    if (playerSymbol === 'X') {
+      setActiveUser('O');
+    } else {
+      setActiveUser('X');
+    }
+  }
+
 
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />          
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activeUser === 'X'}/>
+          <Player initialName="Player 2" symbol="O" isActive={activeUser === 'O'}/>          
         </ol>
-        <GameBoard />
+        <GameBoard playerSymbol={activeUser} handleSquareClick={handleSquareClick}/>
       </div>
       LOG
     </main>
