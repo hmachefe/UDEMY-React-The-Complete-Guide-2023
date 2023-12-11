e805e7e596e6fa15731fd86b39ad92ed2a08dd52
Hugo Machefer <hugo.machefer@difuze.com>
2023-12-11 07:30:25 -0500

    86---Reducing-State-Management---And---Identifying-unnecessary-state

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,20 +3,28 @@ import GameBoard from "./components/Gameboard"
 import { useState } from "react";
 import Log from "./components/Log";
 
+
+function derivatedActiveUser(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activeUser, setActiveUser] = useState('X');
+  // const [activeUser, setActiveUser] = useState('X');
+  const activePlayer = derivatedActiveUser(gameTurns);
 
 
   function handleSelectSquare(rowIndex, colIndex) {    
-    setActiveUser((currentActiveUser => (currentActiveUser === 'X' ? 'O' : 'X')));
+    // setActiveUser((currentActiveUser => (currentActiveUser === 'X' ? 'O' : 'X')));
+    
     setGameTurns((previousGameTurns) => {
 
-      let currentPlayer = 'X';
-
-      if (previousGameTurns.length > 0 && previousGameTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      let currentPlayer = derivatedActiveUser(previousGameTurns);
 
       const updatedGameTurns = [
         {
@@ -39,8 +47,8 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activeUser === 'X'}/>
-          <Player initialName="Player 2" symbol="O" isActive={activeUser === 'O'}/>          
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>          
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
       </div>
