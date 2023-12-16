e7276cb8d4c17a7fbc5d362656c828a886379d47
Hugo Machefer <hugo.machefer@difuze.com>
2023-12-15 23:28:58 -0500

    116---Creating-Reusable-Components---And---Component-Combinations

---
+++ 06_Styling_React_Components/01-starting-project/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  cursor: pointer;
+  background: none;
+  line-height: inherit;
+  &:focus {
+    outline: none;
+  }
+`;
+
+export default Button;
