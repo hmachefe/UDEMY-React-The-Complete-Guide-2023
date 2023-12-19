1811042968479c79eb37b5da45024a01ec38adbb
Hugo Machefer <hugo.machefer@difuze.com>
2023-12-19 14:13:46 -0500

    150---exposing-components-API-via-useImperativeHandleHook

--- 08_Working-With-Refs-And-Portals/01-starting-project/src/components/ResultModal.jsx
+++ 08_Working-With-Refs-And-Portals/01-starting-project/src/components/ResultModal.jsx
@@ -1,22 +1,34 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
-    return (
-        <dialog className="result-modal" ref={ref}>
-            <h2>You {result}</h2>
-            <p>
-                The target time was <strong>{targetTime} seconds.</strong>
-            </p>
-            <p>
-                You stopped the timer with <strong>X seconds left.</strong>
-            </p>
-            <form method="dialog">
-                <button>
-                    Close
-                </button>
-            </form>
-        </dialog>            
-    );
+
+const dialog = useRef();
+
+useImperativeHandle(ref, () => {
+    return {
+        open() {
+            dialog.current.showModal();
+        }
+    }
+});
+
+return (
+    <dialog className="result-modal" ref={dialog}>
+        <h2>You {result}</h2>
+        <p>
+            The target time was <strong>{targetTime} seconds.</strong>
+        </p>
+        <p>
+            You stopped the timer with <strong>X seconds left.</strong>
+        </p>
+        <form method="dialog">
+            <button>
+                Close
+            </button>
+        </form>
+    </dialog>            
+);
+
 });
 
 export default ResultModal;
