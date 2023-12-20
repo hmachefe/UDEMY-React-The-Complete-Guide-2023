3a40f6bfffbbb4c9f16c541595153b28e270b3aa
Hugo Machefer <hugo.machefer@difuze.com>
2023-12-20 09:07:53 -0500

    167---Adding-a-Projects-Sidebar-Component

--- 09_Practice-Project-Project-Management-App-with-Components-State-Refs-And-More/01-starting-project/src/App.jsx
+++ 09_Practice-Project-Project-Management-App-with-Components-State-Refs-And-More/01-starting-project/src/App.jsx
@@ -1,8 +1,11 @@
+import ProjectsSidebar from './components/ProjectsSidebar';
+
 function App() {
+  console.log('App');
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main>
+      <ProjectsSidebar />
+    </main>
   );
 }
 
