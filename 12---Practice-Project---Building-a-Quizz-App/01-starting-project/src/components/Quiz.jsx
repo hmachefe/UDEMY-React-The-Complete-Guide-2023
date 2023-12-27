5d95e7cfcef9ce0f45eb9728277a4ee23a0d0297
Hugo Machefer <hugo.machefer@difuze.com>
2023-12-26 19:51:37 -0500

    235---A-First-Component---And---Some-State

---
+++ 12---Practice-Project---Building-a-Quizz-App/01-starting-project/src/components/Quiz.jsx
@@ -0,0 +1,9 @@
+export default function Quiz() {
+    
+    const [activeQuestion, setActiveQuestion] = useState(0);
+    const [userAnswers, setUserAnswers] = useState([]);
+
+    return (
+        <p>Currently Active Question</p>
+    )
+}
