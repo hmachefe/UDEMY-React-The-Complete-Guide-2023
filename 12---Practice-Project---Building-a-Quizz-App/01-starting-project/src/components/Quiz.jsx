bffb4c9a36d963d1192b2fea1741556ac7c59760
Hugo Machefer <hugo.machefer@difuze.com>
2023-12-27 13:05:54 -0500

    238---Adding-Question-Timers

--- 12---Practice-Project---Building-a-Quizz-App/01-starting-project/src/components/Quiz.jsx
+++ 12---Practice-Project---Building-a-Quizz-App/01-starting-project/src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import QUESTIONS from '../questions.js';
 import quizCompleteImg from '../assets/quiz-complete.png';
+import QuestionTimer from './QuestionTimer.jsx';
 
 export default function Quiz() {
     
@@ -31,6 +32,7 @@ export default function Quiz() {
     return (
         <div id="quiz">
             <div id="question">
+                <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)}/>
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
