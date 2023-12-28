e16504ae6835f622c68495a09d72db17b3a842a6
Hugo Machefer <hugo.machefer@difuze.com>
2023-12-28 18:01:31 -0500

    240---Using-Effect-Cleanup-Functions---And---Using-Keys-for-Reseting-Components

--- 12---Practice-Project---Building-a-Quizz-App/01-starting-project/src/components/QuestionTimer.jsx
+++ 12---Practice-Project---Building-a-Quizz-App/01-starting-project/src/components/QuestionTimer.jsx
@@ -6,17 +6,19 @@ export default function QuestionTimer({ timeout, onTimeout }) {
 
     useEffect(() => {
         console.log('SETTING TIMEOUT');
-        setTimeout(() => {
+        const timeoutId = setTimeout(() => {
             onTimeout();
-        }, timeout);            
+        }, timeout);       
+        return () => clearTimeout(timeoutId);     
     }, [timeout, onTimeout]);
 
 
     useEffect(() => {
         console.log('SETTING INTERVAL');
-        setInterval(() => {
+        const intervalId = setInterval(() => {
             setRemainingTime(previousRemainingTime => previousRemainingTime - 100);
         }, 100);           
+        return () => clearInterval(intervalId);
     }, []);
 
     return <progress id="question-time" value={remainingTime} max={timeout}/>
