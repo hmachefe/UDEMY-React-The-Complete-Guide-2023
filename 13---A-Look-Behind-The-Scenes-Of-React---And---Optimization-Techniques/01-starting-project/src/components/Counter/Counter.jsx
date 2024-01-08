e465c0a00c9ba803bc29cc00effd28856d9694b2
Hugo Machefer <hugo.machefer@difuze.com>
2024-01-07 22:11:18 -0500

    277---Using-Keys-For-Resetting-Components

--- 13---A-Look-Behind-The-Scenes-Of-React---And---Optimization-Techniques/01-starting-project/src/components/Counter/Counter.jsx
+++ 13---A-Look-Behind-The-Scenes-Of-React---And---Optimization-Techniques/01-starting-project/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useMemo, useEffect } from 'react';
+import { useState, useCallback, useMemo /*,  useEffect */ } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
