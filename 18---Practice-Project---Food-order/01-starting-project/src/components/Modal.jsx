bbf60004d794367284cfa2894d8c8306e354d324
hugo.machefer@gmail.com <hugo.machefer@gmail.com>
2024-06-08 21:07:08 -0400

    implement use effect hook inside this model comp

--- 18---Practice-Project---Food-order/01-starting-project/src/components/Modal.jsx
+++ 18---Practice-Project---Food-order/01-starting-project/src/components/Modal.jsx
@@ -1,8 +1,14 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Modal({children, open, className = ''}) {
 
     const dialog = useRef();
+
+    useEffect(() => {
+        if (open) {
+            dialog.current.showModal();
+        }
+    }, [open])
 
     return createPortal(
         <dialog ref={dialog} className={`modal ${className}`}>
