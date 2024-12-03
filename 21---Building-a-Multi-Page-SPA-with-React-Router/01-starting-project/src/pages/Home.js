52a8a8f208829215fe99e0d063e1976896b533cf
hmachefe <hugo.machefer@gmail.com>
2024-12-03 17:13:28 -0500

    navigating between pages with links

--- 21---Building-a-Multi-Page-SPA-with-React-Router/01-starting-project/src/pages/Home.js
+++ 21---Building-a-Multi-Page-SPA-with-React-Router/01-starting-project/src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
-    <div>My Home Page</div>
+    <>
+      <div>My Home Page</div>
+      <Link to="/products">Go to products</Link>
+      {/* <a onClick href="/products">
+        Go to products
+      </a> */}
+    </>
+  
   )
 }
 
