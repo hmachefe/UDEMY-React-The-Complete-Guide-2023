4d98f34fbcb3aaa89fe7caf99c8252184b1ef120
hmachefe <hugo.machefer@gmail.com>
2024-12-05 22:09:39 -0500

    using data from a loader in the route

--- 21---Building-a-Multi-Page-SPA-with-React-Router/new_project_setup/12-adv-starting-project/frontend/src/pages/Events.js
+++ 21---Building-a-Multi-Page-SPA-with-React-Router/new_project_setup/12-adv-starting-project/frontend/src/pages/Events.js
@@ -1,52 +1,11 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
-// import { Link } from 'react-router-dom'
-
-
-// const DUMMY_EVENTS = [
-//     { id: 'e1', title: 'Programming for everyone', description: 'A half-day event.', location: 'Some Street 22, Some City', date: '2021-05-12' },
-//     { id: 'e2', title: 'Networking for introverts', description: 'A half-day event.', location: 'Some Street 22, Some City', date: '2021-05-12' },
-//     { id: 'e3', title: 'Networking for extroverts', description: 'A half-day event.', location: 'Some Street 22, Some City', date: '2021-05-12' },
-// ];
 
 function EventsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
-
+  const events = useLoaderData();
   return (
-    <>
-      <div style={{ textAlign: 'center' }}> 
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
-    </>
+      <EventsList events={events} />
   )
 }
-
-// function EventsPage() {
-//   return (
-//     <ul>
-//       {DUMMY_EVENTS.map((event) => (
-//         <li key={event.id}>
-//           <Link to={event.id}>{event.title}</Link>
-//         </li>
-//       ))}   
-//     </ul>
-//   )
-// }
-
 export default EventsPage;
