a0a985d9988008989d5edfa77f1739c7740c5cd1
Hugo Machefer <hugo.machefer@gmail.com>
2024-12-11 13:36:07 -0500

    managing the token expiration

--- 23---Adding-Authentication-To-React-Apps/01-starting-project/frontend/src/components/Authentication.js
+++ 23---Adding-Authentication-To-React-Apps/01-starting-project/frontend/src/components/Authentication.js
@@ -54,7 +54,10 @@ export async function action({request}) {
   const resData = await response.json();
   const token = resData.token;
   // token needs to be stored. Could be put in memory, or in a cookie
-  localStorage.setItem('token', token);
+  localStorage.setItem("token", token);
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+  localStorage.setItem("expiration", expiration.toISOString());
 
   return redirect("/");
 
