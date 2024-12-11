c573d92a321e47e44f3db8c0c15c913199b45be0
Hugo Machefer <hugo.machefer@gmail.com>
2024-12-11 09:41:34 -0500

    adding route protection

--- 23---Adding-Authentication-To-React-Apps/01-starting-project/frontend/src/util/auth.js
+++ 23---Adding-Authentication-To-React-Apps/01-starting-project/frontend/src/util/auth.js
@@ -1,3 +1,5 @@
+import { redirect } from "react-router-dom";
+
 export function getAuthToken() {
     const token = localStorage.getItem("token");
     return token;
@@ -5,4 +7,13 @@ export function getAuthToken() {
 
 export function tokenLoader() {
     return getAuthToken();
+}
+
+export function checkAuthLoader() {
+    const token = getAuthToken();
+    if (!token) {
+        return redirect("/auth");
+        // or throw an error, up to decision maker
+    }
+    return null; // this is missing in the next lecture video and should be added by you
 }
