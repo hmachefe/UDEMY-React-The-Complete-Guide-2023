73e766780c858c0339fe04e74973dcb791f8b9e8
Hugo Machefer <hugo.machefer@gmail.com>
2024-12-11 15:57:03 -0500

    understanding lazy loading, add lazy loading to blogs and posts

--- 24---Deploying-React-Apps/01-starting-project/src/App.js
+++ 24---Deploying-React-Apps/01-starting-project/src/App.js
@@ -1,14 +1,20 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import BlogPage, { loader as postsLoader } from './pages/Blog';
+import { lazy, Suspense } from 'react';
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
 import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
+// import PostPage, { loader as postLoader } from './pages/Post';
 import RootLayout from './pages/Root';
+
+// const BlogPage = () => import("./pages/Blog"); // returns a promise. Rather than a function returning a JSX code
+const BlogPage = lazy(() => import("./pages/Blog"));
+const PostPage = lazy(() => import("./pages/Post"));
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <RootLayout />,
+    element: (
+      <RootLayout />
+    ),
     children: [
       {
         index: true,
@@ -17,8 +23,20 @@ const router = createBrowserRouter([
       {
         path: 'posts',
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          { 
+            index: true, 
+            element: <Suspense fallback={<p>blog page is loading...</p>}>
+              <BlogPage />
+            </Suspense>,
+            loader: () => import('./pages/Blog').then(module => module.loader()) 
+          },
+          { 
+            path: ':id', 
+            element: <Suspense fallback={<p>post page is loading...</p>}>
+              <PostPage />
+            </Suspense>,            
+            loader: (meta) => import('./pages/Post').then(module => module.loader(meta)) 
+          },
         ],
       },
     ],
