fc013695cbfbd14bc6ed69a85dd70a0b08aff6f7
Hugo Machefer <hugo.machefer@gmail.com>
2024-12-14 20:16:10 -0500

    exercice task solution

---
+++ 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.js
@@ -0,0 +1,9 @@
+// http://localhost:3000/meals/something 
+// will habve precedence over 
+// http://localhost:3000/meals/share
+
+export default function MealDetailsPage() {
+    return <h1>
+        Meal Details
+    </h1>
+}
