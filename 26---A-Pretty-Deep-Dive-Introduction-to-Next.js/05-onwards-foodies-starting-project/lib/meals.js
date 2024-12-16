626633d3234c5c80b8cbc08823183601bd7b614f
Hugo Machefer <Hugo.Machefer@difuze.com>
2024-12-16 09:27:42 -0500

    adding a loading page

---
+++ 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/05-onwards-foodies-starting-project/lib/meals.js
@@ -0,0 +1,12 @@
+import sql from "better-sqlite3";
+
+const db = sql("meals.db");
+
+export async function getMeals() {
+    // new SQL statement that should be executed
+    // db.prepare("SELECT * FROM meals").run(); // run is used for inserting data, or changing them
+    // db.prepare("SELECT * FROM meals").get(); // would be used for one single data
+
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // artificial, demo purpose
+    return db.prepare("SELECT * FROM meals").all(); // all is used for fetching data
+}
