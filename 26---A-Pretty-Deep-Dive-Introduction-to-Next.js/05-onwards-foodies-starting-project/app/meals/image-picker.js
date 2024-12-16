15402f6374c19f1cdea56e1678ed1aa61fbc8376
Hugo Machefer <Hugo.Machefer@difuze.com>
2024-12-16 16:58:13 -0500

    adding an image preview to the picker

--- 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/05-onwards-foodies-starting-project/app/meals/image-picker.js
+++ 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/05-onwards-foodies-starting-project/app/meals/image-picker.js
@@ -1,11 +1,27 @@
 "use client";
-
+import { useState } from "react";
 import { useRef } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 export default function ImagePicker( { label, name } ) {
-
+    // requires to turn this originally server based comp into a client one, done with "use client";
+    const [pickedImage, setPickedImage] = useState(); 
     const imageInput = useRef();
+
+    function handleImageChange(event) {
+        const file = event.target.files[0]; /* since multiple attribute not set for <input type="file" ... */
+        if (!file) {
+            setPickedImage(null);
+            return;
+        }
+
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result);
+        };
+        fileReader.readAsDataURL(file);
+    }
 
     function handlePickClick() {
         imageInput.current.click(); // do not forget to use .current because of useRef()
@@ -16,6 +32,14 @@ export default function ImagePicker( { label, name } ) {
             {label}
         </label>
         <div className={classes.controls}>
+            <div className={classes.preview}>
+                {!pickedImage && <p>No impage picked yet. Empty preview</p>}
+                {pickedImage && <Image 
+                    src={pickedImage} 
+                    alt="The image selected by the user"
+                    fill
+                />}
+            </div>
             <input 
                 className={classes.input}
                 type="file" 
@@ -23,6 +47,8 @@ export default function ImagePicker( { label, name } ) {
                 accept="image/png, image/jpg" 
                 name={name}
                 ref={imageInput}
+                /* multiple */ 
+                onChange={handleImageChange}
             />
             <button 
                 className={classes.button}
