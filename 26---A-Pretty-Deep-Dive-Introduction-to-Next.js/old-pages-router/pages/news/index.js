08cc6237361222cf25a5b4a7ac7f64a3f8cea3c0
Hugo Machefer <hugo.machefer@gmail.com>
2024-12-18 15:50:43 -0500

    fixing but due to typo

--- 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/old-pages-router/pages/news/index.js
+++ 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/old-pages-router/pages/news/index.js
@@ -2,14 +2,14 @@
 // our-domain/something--important is possible, provided that <news> exists as an intermediate folder
 // no need to import React from "react"; Can be omitted. Added behind the scenes
 import { Fragment } from "react";
-import Link from "net/link"
+import Link from "next/link";
 
 function NewPage() {
     return <Fragment>
         <h1>
             <ul>
                 <li>
-                    <Link href="/next/next-js-is-a-great-framework">
+                    <Link href="/news/next-js-is-a-great-framework">
                         Next JS is a great framework
                     </Link>
                 </li>
