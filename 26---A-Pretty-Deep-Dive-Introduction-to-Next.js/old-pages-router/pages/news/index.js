38af5f9bfeb2e6b4d057520c3eb8dabf3dbecc16
Hugo Machefer <hugo.machefer@gmail.com>
2024-12-18 15:20:04 -0500

    adding nested paths and pages

--- 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/old-pages-router/pages/news/index.js
+++ 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/old-pages-router/pages/news/index.js
@@ -1,4 +1,5 @@
 // our-domain/news (not news.js but news then serving the news.js file)
+// our-domain/something--important is possible, provided that <news> exists as an intermediate folder
 
 // no need to import React from "react"; Can be omitted. Added behind the scenes
 
