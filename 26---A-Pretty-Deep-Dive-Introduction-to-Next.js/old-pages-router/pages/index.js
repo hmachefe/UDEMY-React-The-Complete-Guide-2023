4150a9d76a92d84c42b6771042123594950c127e
Hugo Machefer <hugo.machefer@gmail.com>
2024-12-18 14:40:13 -0500

    adding first pages

---
+++ 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/old-pages-router/pages/index.js
@@ -0,0 +1,10 @@
+// our-domain/ (nothing after slash terminating character)
+
+function HomePage() {
+  return <h1>
+      The Home Page
+  </h1>
+}
+
+
+export default HomePage;
---
+++ 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/old-pages-router/pages/news.js
@@ -0,0 +1,12 @@
+// our-domain/news (not news.js but news then serving the news.js file)
+
+// no need to import React from "react"; Can be omitted. Added behind the scenes
+
+function NewPage() {
+    return <h1>
+        The News Page
+    </h1>
+  }
+  
+  
+  export default NewPage;
