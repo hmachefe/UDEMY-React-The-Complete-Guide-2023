37ac4aefcc2603432ef23bf4d2693e383941c596
Hugo Machefer <hugo.machefer@gmail.com>
2024-12-18 15:48:56 -0500

    linking between pages with Link tag provided by next/link and href attribute

--- 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/old-pages-router/pages/news/index.js
+++ 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/old-pages-router/pages/news/index.js
@@ -1,12 +1,24 @@
 // our-domain/news (not news.js but news then serving the news.js file)
 // our-domain/something--important is possible, provided that <news> exists as an intermediate folder
-
 // no need to import React from "react"; Can be omitted. Added behind the scenes
+import { Fragment } from "react";
+import Link from "net/link"
 
 function NewPage() {
-    return <h1>
-        The News Page
-    </h1>
+    return <Fragment>
+        <h1>
+            <ul>
+                <li>
+                    <Link href="/next/next-js-is-a-great-framework">
+                        Next JS is a great framework
+                    </Link>
+                </li>
+                <li>
+                    Something Else
+                </li>
+            </ul>
+        </h1>
+    </Fragment>
   }
   
   
