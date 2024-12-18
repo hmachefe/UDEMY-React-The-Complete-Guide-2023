0c8f04b1d9f7672ef1b7119e4527dcb9f7ba01c5
Hugo Machefer <hugo.machefer@gmail.com>
2024-12-18 15:39:02 -0500

    extracting dynamic parameter values

--- 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/old-pages-router/pages/news/[newsId].js
+++ 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/old-pages-router/pages/news/[newsId].js
@@ -1,8 +1,15 @@
 // our-domain/something-important
+import { useRouter} from "next/router"
 
 function DetailPage() {
+
+    const router = useRouter();
+    console.log(router.query.newsId);
+
     return <h1>
-        The Detail Page
+        The Detail Page about news ID 
+        <br />
+        {router.query.newsId}
     </h1>
   }
   
