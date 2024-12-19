02f147955473a438679526bee799955b86e5eec6
Hugo Machefer <hugo.machefer@gmail.com>
2024-12-19 16:10:04 -0500

    sending http requests to our api routes

--- 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/06-onwards-to-a-bigger-project-starter/pages/new-meetup/index.js
+++ 26---A-Pretty-Deep-Dive-Introduction-to-Next.js/06-onwards-to-a-bigger-project-starter/pages/new-meetup/index.js
@@ -4,8 +4,16 @@ import NewMeetUpForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
 
-    function addMeetupHandler(enteredMeetupData) {
-        console.log(enteredMeetupData)
+    async function addMeetupHandler(enteredMeetupData) {
+        const response = await fetch("/api/new-meetup", {
+            method: "POST",
+            body: JSON.stringify(enteredMeetupData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await response.json();
+        console.log(data);
     }
 
     return <NewMeetUpForm onAddMeetup={addMeetupHandler}/>
