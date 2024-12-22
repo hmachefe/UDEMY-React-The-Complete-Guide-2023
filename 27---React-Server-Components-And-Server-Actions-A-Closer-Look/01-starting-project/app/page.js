77fd238c5a31c6df5508ba032998d20e9c9cc59c
Hugo Machefer <hugo.machefer@gmail.com>
2024-12-22 12:54:13 -0500

    using the Use() hook for PRomises And Data fetching

--- 27---React-Server-Components-And-Server-Actions-A-Closer-Look/01-starting-project/app/page.js
+++ 27---React-Server-Components-And-Server-Actions-A-Closer-Look/01-starting-project/app/page.js
@@ -1,16 +1,24 @@
 // import RSCDemo from "@/components/RSCDemo";
 // import ClientDemo from "./components/ClientDemo";
-import DataFetchingDemo from "./components/DataFetchingDemo";
-import ServerActionsDemo from "./components/ServerActionDemo";
+// import DataFetchingDemo from "./components/DataFetchingDemo";
+// import ServerActionsDemo from "./components/ServerActionDemo";
+import UsePromiseDemo from "./components/UsePromisesDemo";
+import fs from "node:fs/promises";
 
-export default function Home() {
+export default async function Home() {
+
+  const data = await fs.readFile("dummy-db.json", "utf-8");
+  const users = JSON.parse(data);
+  console.log(users);
+
   return (
     <main>
       {/* <ClientDemo>
         <RSCDemo />
       </ClientDemo> */}
       { /* <DataFetchingDemo /> */}
-      <ServerActionsDemo />
+      { /* <ServerActionsDemo />*/ }
+      <UsePromiseDemo users={users}/>
     </main>
   );
 }
