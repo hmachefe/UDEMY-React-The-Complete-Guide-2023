e68424a7804dbd2a5e89dd690389ed828e0148ac
Hugo Machefer <hugo.machefer@gmail.com>
2024-12-25 19:07:48 -0500

    ad again previous commented lines for history purpose

--- 27---React-Server-Components-And-Server-Actions-A-Closer-Look/01-starting-project/app/page.js
+++ 27---React-Server-Components-And-Server-Actions-A-Closer-Look/01-starting-project/app/page.js
@@ -1,14 +1,24 @@
+// import RSCDemo from "@/components/RSCDemo";
+// import ClientDemo from "./components/ClientDemo";
+// import DataFetchingDemo from "./components/DataFetchingDemo";
+// import ServerActionsDemo from "./components/ServerActionDemo";
 import fs from "node:fs/promises";
 import { Suspense } from "react";
 import UsePromiseDemo from "./components/UsePromisesDemo";
 import ErrorBoundary from "./components/ErrorBoundary"; // Import fixed ErrorBoundary
 
 export default async function Home() {
+
   const fetchUsersPromise = new Promise((resolve, reject) => 
     setTimeout(() => {
       reject(new Error("Artificial Error for Testing!")); // Artificial error
     }, 3000)
   );
+  
+
+  // const data = await fs.readFile("dummy-db.json", "utf-8");
+  // const users = JSON.parse(data);
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
 
   return (
     <main>
