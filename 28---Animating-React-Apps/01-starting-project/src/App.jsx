aa128257dcccc2400d1a53a20bc496d876ff72c8
Hugo Machefer <Hugo.Machefer@difuze.com>
2024-12-26 12:03:39 -0500

    framer motion basics and fundamentals

--- 28---Animating-React-Apps/01-starting-project/src/App.jsx
+++ 28---Animating-React-Apps/01-starting-project/src/App.jsx
@@ -1,15 +1,55 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { useState } from 'react';
 
-import WelcomePage from './pages/Welcome.jsx';
-import ChallengesPage from './pages/Challenges.jsx';
-
-const router = createBrowserRouter([
-  { path: '/', element: <WelcomePage /> },
-  { path: '/challenges', element: <ChallengesPage /> },
-]);
+import { motion } from 'framer-motion';
 
 function App() {
-  return <RouterProvider router={router} />;
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [rotate, setRotate] = useState(0);
+
+  return (
+    <div id="demo">
+      <motion.div 
+        id="box" 
+        // animate = {{x: x}}
+        animate = {{x, y, rotate}}
+        transition={{
+          duration: 3,
+          type: "spring", // "tween"
+          // bounce: false
+        }}
+      />
+
+      <div id="inputs">
+        <p>
+          <label htmlFor="x">X</label>
+          <input
+            type="number"
+            id="x"
+            onChange={(event) => setX(+event.target.value)}
+          />
+        </p>
+
+        <p>
+          <label htmlFor="y">Y</label>
+          <input
+            type="number"
+            id="y"
+            onChange={(event) => setY(+event.target.value)}
+          />
+        </p>
+
+        <p>
+          <label htmlFor="rotate">Rotate</label>
+          <input
+            type="number"
+            id="rotate"
+            onChange={(event) => setRotate(+event.target.value)}
+          />
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
