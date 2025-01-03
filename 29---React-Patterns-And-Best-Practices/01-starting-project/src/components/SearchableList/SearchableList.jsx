05a53bc50508ea6c0f992b2a8c07af66a746bd8a
Hugo Machefer <Hugo.Machefer@difuze.com>
2025-01-02 22:40:27 -0500

    introducing and using render props

---
+++ 29---React-Patterns-And-Best-Practices/01-starting-project/src/components/SearchableList/SearchableList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function SearchableList({items}) {
+  return (
+    <div className="searchable-list">
+        <input type="search" placeholder='Search' />
+        <ul>
+            {items.map((item, index) => (
+                <li key={item}>
+                    {item.toString()}
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default SearchableList
