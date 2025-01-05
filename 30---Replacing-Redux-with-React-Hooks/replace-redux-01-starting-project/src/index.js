ad20cf2c2598a2b616b4b9fa097362f271299b3a
Hugo Machefer <hugo.machefer@gmail.com>
2025-01-05 12:17:31 -0500

    remove line breaks

--- 30---Replacing-Redux-with-React-Hooks/replace-redux-01-starting-project/src/index.js
+++ 30---Replacing-Redux-with-React-Hooks/replace-redux-01-starting-project/src/index.js
@@ -6,7 +6,6 @@ import './index.css';
 import App from './App';
 import configureProductsStore from './hooks-store/products-store';
 
-
 configureProductsStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
