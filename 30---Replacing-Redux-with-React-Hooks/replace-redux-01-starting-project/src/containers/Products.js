0205d869212e0f16f25da0b37842ba2d2949be85
Hugo Machefer <hugo.machefer@gmail.com>
2025-01-05 11:55:26 -0500

    using the custom store

--- 30---Replacing-Redux-with-React-Hooks/replace-redux-01-starting-project/src/containers/Products.js
+++ 30---Replacing-Redux-with-React-Hooks/replace-redux-01-starting-project/src/containers/Products.js
@@ -1,16 +1,19 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
 import ProductItem from '../components/Products/ProductItem';
-import { ProductsContext } from '../context/products-context';
 import './Products.css';
+import { useStore } from '../hooks-store/store';
 
 const Products = props => {
 
-  const productsList = useContext(ProductsContext).products;
+  /* 
+  const [state, dispatch] = useStore();
+  */
+  const state = useStore()[0];
 
   return (
     <ul className="products-list">
-      {productsList.map(prod => (
+      {state.products.map(prod => (
         <ProductItem
           key={prod.id}
           id={prod.id}
