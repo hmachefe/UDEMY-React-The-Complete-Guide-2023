b8fc7f63b8cd1c7bb5eb44fe6074bdeee96895ab
Hugo Machefer <hugo.machefer@gmail.com>
2025-01-05 12:16:48 -0500

    re-indent

--- 30---Replacing-Redux-with-React-Hooks/replace-redux-01-starting-project/src/hooks-store/store.js
+++ 30---Replacing-Redux-with-React-Hooks/replace-redux-01-starting-project/src/hooks-store/store.js
@@ -5,7 +5,7 @@ let listeners = [];
 let actions = {};
 
 export const useStore = () => {
-  const setState = useState(globalState)[1];
+    const setState = useState(globalState)[1];
 
     const dispatch = (actionIdentifier, payload) => {
         const newState = actions[actionIdentifier](globalState, payload);
