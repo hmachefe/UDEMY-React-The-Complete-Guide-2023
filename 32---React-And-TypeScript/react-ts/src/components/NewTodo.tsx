ffc8bdb13af7dff8e7a03e83c27cf35c194b18ce
Hugo Machefer <hugo.machefer@gmail.com>
2025-01-11 09:08:14 -0500

    time to practice exercise time, create TodoItem component as a new comp embraging, again,t TS

---
+++ 32---React-And-TypeScript/react-ts/src/components/NewTodo.tsx
@@ -0,0 +1,11 @@
+const NewTodo = () => {
+    return <form>
+         <label>
+            Todo Text
+         </label>
+         <input type="text" />
+         <button>Add Todo</button>
+    </form>
+}
+
+export default NewTodo;     
--- 32---React-And-TypeScript/react-ts/src/components/Todos.tsx
+++ 32---React-And-TypeScript/react-ts/src/components/Todos.tsx
@@ -1,9 +1,12 @@
 import Todo from "../models/Todo";
+import TodoItem from "./TodoItem";
 
 // function Todos(props: string[], children) {
 const Todos: React.FC<{items: Todo[]}> = (props) => {
     return <ul>
-        {props.items.map(item => <li key={item.id}>{item.text}</li>)}
+        {props.items.map(item => (
+            <TodoItem key={item.id} text={item.text} />
+        ))}
     </ul>
 }
 
