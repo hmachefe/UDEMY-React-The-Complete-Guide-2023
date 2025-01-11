68108d9c9ed811141272386870bdca4ec272bf58
Hugo Machefer <hugo.machefer@gmail.com>
2025-01-11 14:30:26 -0500

    working with function props

--- 32---React-And-TypeScript/react-ts/src/App.tsx
+++ 32---React-And-TypeScript/react-ts/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import NewTodo from './components/NewTodo'
 import Todos from './components/Todos'
 import Todo from './models/Todo'
 
@@ -9,8 +10,13 @@ function App() {
     new Todo('learn TypeScript')
   ]
 
+  const addTodoHandler = (todoText: string) => {
+
+  };
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos}/>
     </div>
   )
